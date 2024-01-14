f6487268b7f7bac3ef0bf32861a4b9f0c428b370
4sizn <4sizn@naver.com>
2024-01-14 20:56:39 +0900

    feat: ,

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,83 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import viteLogo from "/vite.svg";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <canvas id="canvas"></canvas>
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const $canvas = document.querySelector("canvas");
+if (!$canvas) throw new Error("canvas not found");
+
+const ctx = $canvas.getContext("2d");
+if (!ctx) throw new Error("context not found");
+
+const colors = [
+  "#dc0936",
+  "#e6471d",
+  "#f7a416",
+  "#efe61f ",
+  "#60b236",
+  "#209b6c",
+  "#169ed8",
+  "#3f297e",
+  "#87207b",
+  "#be107f",
+  "#e7167b",
+];
+
+const itemList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+type Position = {
+  x: number;
+  y: number;
+};
+
+const center: Position = {
+  x: $canvas.width / 2,
+  y: $canvas.height / 2,
+};
+
+const draw = () => {
+  const arc = Math.PI / (itemList.length / 2);
+  for (let i = 0; i < itemList.length; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = colors[i % (colors.length - 1)];
+    ctx.moveTo(center.x, center.y);
+    ctx.arc(center.x, center.y, center.x, arc * (i - 1), arc * i);
+    ctx.fill();
+    ctx.closePath();
+  }
+};
+
+const drawPointer = (position: Position) => {
+  const { x, y } = position;
+  // 포인터 스타일 설정
+  ctx.fillStyle = "#FF0000"; // 빨간색
+  ctx.strokeStyle = "#000000"; // 검은색
+  ctx.lineWidth = 2;
+
+  // 포인터 그리기
+  ctx.beginPath();
+  ctx.moveTo(x, y + 30);
+  ctx.lineTo(x - 10, y);
+  ctx.lineTo(x + 10, y);
+  ctx.closePath();
+
+  ctx.fill();
+  ctx.stroke();
+
+  // // 원 모양의 머리 부분 그리기
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
+  ctx.closePath();
+
+  ctx.fill();
+  ctx.stroke();
+};
+
+draw();
+drawPointer({
+  x: center.x,
+  y: 0,
+});
